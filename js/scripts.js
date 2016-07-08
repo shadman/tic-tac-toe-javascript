e28ebf6f829b5fc9e07c307248e2e3d6dec05e5b
Shadman <shadman.jamil@tenpearls.com>
2016-07-09 00:30:22 +0500

    added winning patterns of 3x3; added logic for 2 manual players

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,119 @@
-var turn = 'X';
 
+var turn = 'X';
+var game_type = 3;
+var total_turns = 0;
+
+var selections = new Array(); 
+selections['X'] = new Array();
+selections['Y'] = new Array();
+
+// Resetting parameters on reseting game
+function resetParams() {
+	turn = 'X';
+	game_type = '3';
+	total_turns = 0;
+
+	selections['X'] = new Array();
+	selections['Y'] = new Array();
+}
+
+// Change turn after another
+function changeTurn(){
+	if (turn == 'X') turn = 'Y';
+	else turn = 'X';
+}
+
+// Winner patterns, match selected patterns on every turn
+function winnerPatterns() {
+	var wins = Array();
+
+	// 3 x 3 winning patterns;
+	if (game_type==3) wins = [ 
+								[11,12,13], [21,22,23], [31,32,33],
+						 		[11,21,31], [12,22,32], [13,23,33], 
+						 		[11,22,33], [13,22,31]
+						 	];
+
+	return wins
+}
+
+// Checking winner of selected type on selection
+function checkWinner() {
+
+	var finished = false;
+
+	var selected = selections[turn].sort();
+	var win_patterns = winnerPatterns();
+
+	for (var x=0; x < win_patterns.length; x++) {
+		
+		if (finished != true) { 
+			finished = isWinner(win_patterns[x], selections[turn]);
+
+			if ( finished === true ) {
+				alert(turn+' Wins !!');
+
+				// On winning disabled all boxes
+				disableAllBoxes();
+				break;
+			} 
+		}
+	}
+
+	// If no one wins; declare DRAW
+	if ( (total_turns == game_type*3) && finished === false ) alert('Draw!');
+}
+
+// Verifying each selections with winning pattern
+function isWinner(win_pattern, selections){
+
+	var match = 0;
+
+	for (var x=0; x<win_pattern.length; x++) {
+		for (var y=0; y<selections.length; y++) {
+			if (win_pattern[x]==selections[y]) {
+				match++;
+			}
+		}
+	}
+	console.log(match);
+	if (match==win_pattern.length) return true;
+
+	return false;
+}
+
+// Disable all boxes after winning/draw
+function disableAllBoxes() {
+
+	var elements = document.getElementsByClassName("grid-box");
+	for (var i = 0; i < elements.length; i++) {
+	  elements[i].disabled =true;
+	}
+
+}
+
+// Generating a board for new game
 function generateGame(){
 
-	// Getting Variables
-	var game_type = document.getElementById('game_type').value;
+	// Reseting all initialized params as user selected new game
+	resetParams();
+
+	// Getting Variables to update global param
+	game_type = Number(document.getElementById('game_type').value);
 
 	// Clearing board for new game
 	document.getElementById('game-board').innerHTML = '';
 
 	// Generating board
-	for (x=0; x<game_type; x++){
-		for (y=0; y<game_type; y++) {
+	for (var row=1; row<=game_type; row++){
+		for (var col=1; col<=game_type; col++) {
+			var unique_name = 'grid-'+row+'-'+col;
+			var unique_id = row+''+col;
 			var button = document.createElement("input");
-			button.setAttribute("value", ' ');
-			button.setAttribute("name", 'grid');
+
+			button.setAttribute("value", '');
+			button.setAttribute("id", unique_id);
+			button.setAttribute("name", unique_name);
 			button.setAttribute("class", 'grid-box');
 			button.setAttribute("type", 'button');
 			button.setAttribute("onclick", "markCheck(this)");
@@ -25,9 +125,11 @@ function generateGame(){
 
 }
 
-
+// Selecting check for desired position
 function markCheck(obj){
+
 	obj.value = turn;
+	total_turns++;
 
 	if (turn == 'X' ) {
 		obj.setAttribute("class", 'green-player');
@@ -36,10 +138,14 @@ function markCheck(obj){
 	}
 
 	obj.setAttribute("disabled", 'disabled');
+	selections[turn].push(Number(obj.id));
+	console.log(turn+''+obj.id);
+
+	checkWinner();
 	changeTurn();
+
 }
 
-function changeTurn(){
-	if (turn == 'X') turn = 'Y';
-	else turn = 'X';
-}
+
+
+
