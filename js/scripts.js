0fec0be890160ebdf26938797d7923133c2e17c0
Shadman <shadman.jamil@tenpearls.com>
2016-07-08 19:57:44 +0500

    updated css and added turns functiionality

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,34 @@ function generateGame(){
 	for (x=0; x<game_type; x++){
 		for (y=0; y<game_type; y++) {
 			var button = document.createElement("input");
-			button.setAttribute("value", '');
+			button.setAttribute("value", ' ');
 			button.setAttribute("name", 'grid');
 			button.setAttribute("class", 'grid-box');
 			button.setAttribute("type", 'button');
-			//button.setAttribute("onclick", alert("blabla"));
+			button.setAttribute("onclick", "markCheck(this)");
 			document.getElementById('game-board').appendChild(button);
 		}
 		var breakline = document.createElement("br");
 			document.getElementById('game-board').appendChild(breakline);
 	}
 
+}
+
+
+function markCheck(obj){
+	obj.value = turn;
+
+	if (turn == 'X' ) {
+		obj.setAttribute("class", 'green-player');
+	} else {
+		obj.setAttribute("class", 'red-player');
+	}
+
+	obj.setAttribute("disabled", 'disabled');
+	changeTurn();
+}
+
+function changeTurn(){
+	if (turn == 'X') turn = 'Y';
+	else turn = 'X';
 }
